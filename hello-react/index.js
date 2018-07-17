2057e32ae85c4cf034489da35ef0d183d51ad4bd
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-17 10:58:45 -0400

    re-arrange order of variables to render to screen

--- hello-react/index.js
+++ hello-react/index.js
@@ -8,8 +8,19 @@
 // });
 
 
-
+const h = React.createElement;
 
 //React version
-let hello = React.createElement('h1', null, 'hello react!');
-ReactDOM.render(hello, document.querySelector('[data-root]'));
+let hello = React.createElement('h1', null, 'Kebabs!');
+
+let item = React.createElement('li', null, 'chicken hearts');
+let list = React.createElement('ul', null, item);
+
+let main = React.createElement('div', null, [
+    hello,
+    list
+]);
+
+
+
+ReactDOM.render(main, document.querySelector('[data-root]'));
